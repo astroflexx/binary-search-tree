17c03c56340990af485bb5bca3c71561f9df4806
bananabread08 <rmkfermin@gmail.com>
2022-08-11 12:01:30 +0800

    create isBalanced() and rebalance() methods

--- Tree.js
+++ Tree.js
@@ -2,11 +2,25 @@ const Node = require('./Node');
 
 class Tree {
   constructor(array) {
-    const sorted = [...new Set(array)].sort((a, b) => a - b);
-    this.root = this.buildTree(sorted);
+    this.root = this.buildTree(array);
   }
 
-  buildTree(sorted) {
+  #sortAndRemoveDupes(array) {
+    const sorted = [...new Set(array)].sort((a, b) => a - b);
+    return sorted;
+  }
+
+  #minValue(root) {
+    let minv = root.key;
+    while (root.left != null) {
+      minv = root.left.key;
+      root = root.left;
+    }
+    return minv;
+  }
+
+  buildTree(array) {
+    let sorted = this.#sortAndRemoveDupes(array);
     if (sorted.length === 0) return null;
     const mid = parseInt(sorted.length / 2);
     const root = new Node(
@@ -15,15 +29,6 @@ class Tree {
       this.buildTree(sorted.slice(mid + 1))
     );
     return root;
-  }
-
-  static minValue(root) {
-    let minv = root.key;
-    while (root.left != null) {
-      minv = root.left.key;
-      root = root.left;
-    }
-    return minv;
   }
 
   insert(value, root = this.root) {
@@ -42,7 +47,7 @@ class Tree {
     else {
       if (root.left === null) return root.right;
       else if (root.right === null) return root.left;
-      root.key = this.minValue(root.right);
+      root.key = this.#minValue(root.right);
       root.right = this.delete(value, root.right);
     }
     return root;
@@ -66,6 +71,7 @@ class Tree {
   // Breadth First Traversal (level by level)
   // use Queue : First In, First Out
   levelOrder(callback) {
+    if (!this.root) return [];
     const queue = [this.root];
     const results = [];
     while (queue.length) {
@@ -92,6 +98,7 @@ class Tree {
 
   // root left right
   preorder(callback) {
+    if (!this.root) return [];
     const stack = [this.root];
     const results = [];
     while (stack.length) {
@@ -106,6 +113,7 @@ class Tree {
 
   // left root right
   inorder(node = this.root, callback, result = []) {
+    if (!this.root) return [];
     if (node === null) return;
     this.inorder(node.left, callback, result);
     callback ? callback(node) : result.push(node.key);
@@ -115,6 +123,7 @@ class Tree {
 
   // left right root
   postorder(callback) {
+    if (!this.root) return [];
     const stack = [this.root];
     const results = [];
     while (stack.length) {
@@ -138,21 +147,43 @@ class Tree {
     if (!node) return null;
     if (root === null) return 0;
     if (root.key === node.key) return level;
-    let ans = this.depth(node, root.left, level + 1);
-    if (ans !== 0) return ans;
+    let count = this.depth(node, root.left, level + 1);
+    if (count !== 0) return count;
     return this.depth(node, root.right, level + 1);
+  }
+
+  isBalanced(root = this.root) {
+    const checkHeight = (node) => {
+      if (node === null) return 0;
+      const left = checkHeight(node.left);
+      const right = checkHeight(node.left);
+      if (left === false || right === false || Math.abs(left - right) > 1)
+        return false;
+      return Math.max(left, right) + 1;
+    };
+
+    if (root === null) return true;
+    return checkHeight(root) !== false;
+  }
+
+  rebalance() {
+    if (this.root === null) return;
+    const sorted = [...new Set(this.inorder().sort((a, b) => a - b))];
+    this.root = this.buildTree(sorted);
   }
 }
 
-let tree = new Tree([1, 2, 4, 3, 5, 6, 7]);
-//console.log(tree.find(6));
-//console.log(tree.insert(8));
-//console.log(tree.find(8));
+// TESTS
+let tree = new Tree([1, 3, 2, 4]);
+console.log(tree.find(6)); //null
+console.log(tree.insert(5));
+console.log(tree.find(5));
 console.log(tree);
-console.log(tree.levelOrder());
-console.log(tree.preorder());
-console.log(tree.inorder());
-console.log(tree.postorder());
-console.log(tree.height(tree.find(5)));
-console.log(tree.depth(tree.find(6)));
-// console.log(tree.isBalanced());
+console.log(tree.levelOrder()); //[ [ 3 ], [ 2, 4 ], [ 1, 5 ] ]
+console.log(tree.preorder()); // [ 3, 2, 1, 4, 5 ]
+console.log(tree.inorder()); //[ 1, 2, 3, 4, 5 ]
+console.log(tree.postorder()); //[ 1, 2, 5, 4, 3 ]
+console.log(tree.height(tree.find(5))); // 1
+console.log(tree.depth(tree.find(6))); // null
+console.log(tree.isBalanced()); // true;
+console.log(tree.rebalance());
